90ccf47539d12756f946982eb93ca5e1af76b935
cesarucal <cesar.garciar1@ucalgary.ca>
2024-03-19 14:50:55 -0600

    partial 1 v2

--- lab8/ex1.py
+++ lab8/ex1.py
@@ -10,33 +10,58 @@ class Graph:
     def addNode(self, data):
         node = GraphNode(data)
         self.nodes[data] = node
-        self.edges[node] = []
+        self.edges[data] = {}
         return node
 
     def removeNode(self, node):
         del self.nodes[node.data]
-        for edge in self.edges.values():
-            edge[:] = [x for x in edge if x[0] != node]
-        del self.edges[node]
+        del self.edges[node.data]
+        for edge_list in self.edges.values():
+            if node.data in edge_list:
+                del edge_list[node.data]
 
     def addEdge(self, n1, n2, weight=1):
-        self.edges[n1].append((n2, weight))
-        self.edges[n2].append((n1, weight))
+        if n1.data not in self.nodes or n2.data not in self.nodes:
+            raise ValueError("Both nodes must exist in the graph")
+        self.edges[n1.data][n2.data] = weight
+        self.edges[n2.data][n1.data] = weight
 
     def removeEdge(self, n1, n2):
-        self.edges[n1][:] = [x for x in self.edges[n1] if x[0] != n2]
-        self.edges[n2][:] = [x for x in self.edges[n2] if x[0] != n1]
+        if n1.data not in self.nodes or n2.data not in self.nodes:
+            raise ValueError("Both nodes must exist in the graph")
+        del self.edges[n1.data][n2.data]
+        del self.edges[n2.data][n1.data]
 
     def importFromFile(self, file):
+        self.nodes = {}
+        self.edges = {}
         with open(file, 'r') as f:
             lines = f.readlines()
-        if not lines[0].startswith('strict graph'):
+        if not lines:
             return None
-        self.nodes.clear()
-        self.edges.clear()
-        for line in lines[1:-1]:
-            n1, _, n2, weight = line.split()
-            weight = int(weight[8:-1]) if 'weight' in weight else 1
-            n1 = self.addNode(n1)
-            n2 = self.addNode(n2)
-            self.addEdge(n1, n2, weight)
+
+        if not lines[0].startswith("strict graph"):
+            return None
+
+        for line in lines[1:]:
+            if line.strip() and not line.startswith("//") and not line.startswith("}"):
+                parts = line.split("--")
+                if len(parts) != 2:
+                    return None
+                node1, node2 = parts
+                node1 = node1.strip()
+                node2_parts = node2.strip().split()
+                if len(node2_parts) == 1:
+                    node2 = node2_parts[0].strip(";")
+                    weight = 1
+                elif len(node2_parts) == 3 and node2_parts[1] == "[weight=":
+                    node2 = node2_parts[0].strip()
+                    weight = int(node2_parts[2].strip("];"))
+                else:
+                    return None
+                if node1 not in self.nodes:
+                    self.addNode(node1)
+                if node2 not in self.nodes:
+                    self.addNode(node2)
+                self.addEdge(self.nodes[node1], self.nodes[node2], weight)
+        return self
